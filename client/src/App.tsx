c307667ce90102f8edd0af941142ee83c310f215
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-05-13 12:04:24 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div>
+      <p className=' text-orange-500'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam corrupti
+        obcaecati ea nisi, numquam quasi veniam doloremque illo eius, deserunt
+        amet eaque consequuntur optio ratione? Aspernatur commodi ipsa tenetur
+        aliquid!
+      </p>
+      <Button>Hello There</Button>
+    </div>
+  );
+}
+
+export default App;
