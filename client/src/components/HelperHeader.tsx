81812829ae1f69a11fc16161fbf6e2aa918e009c
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-05-18 15:07:59 +0530

    "Added axios dependency and implemented code saving functionality in HelperHeader component"

--- client/src/components/HelperHeader.tsx
+++ client/src/components/HelperHeader.tsx
@@ -13,8 +13,23 @@ import {
   updateCurrentLanguage,
 } from '@/redux/slices/compilerSlice';
 import { RootState } from '@/redux/store';
+import { handleError } from '@/utils/handleError';
+import axios from 'axios';
 
 export default function HelperHeader() {
+  const fullCode = useSelector(
+    (state: RootState) => state.compilerSlice.fullCode
+  );
+  const handleSaveCode = async () => {
+    try {
+      const response = await axios.post('http://localhost:4000/compiler/save', {
+        fullCode: fullCode,
+      });
+      console.log(response.data);
+    } catch (error) {
+      handleError(error);
+    }
+  };
   const dispatch = useDispatch();
   const currentLanguage = useSelector(
     (state: RootState) => state.compilerSlice.currentLanguage
@@ -23,6 +38,7 @@ export default function HelperHeader() {
     <div className='flex items-center justify-between h-12 p-2 text-white bg-black __header_helper'>
       <div className='flex gap-1 __btn_container'>
         <Button
+          onClick={handleSaveCode}
           className='flex items-center justify-center gap-1 bg-green-500 hover:bg-green-700'
           variant='outline'
         >
